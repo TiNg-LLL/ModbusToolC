cc0a76464e4c9135415007b2f5b0f4f29b9a876e
bin33 <122081846@qq.com>
2020-12-27 12:44:39 +0800

    添加项目文件。

---
+++ PanelCollection/ResgiterCollection.cs
@@ -0,0 +1,38 @@
+﻿using Panel_Unit.Register.Common;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Panel_Collection.Register.Collection
+{
+    public class ResgisterCollection : TableLayoutPanel
+    {
+        //创建Register集合
+        public static List<ResgisterCommon> resgisterList = new List<ResgisterCommon>();
+
+        public static int resgisterAmount = 5;
+
+        public ResgisterCollection()
+        {
+            //在集合中创建对应数量的对象
+            for (int i = 1; i <= resgisterAmount; i++)
+            {
+                resgisterList.Add(new ResgisterCommon());
+                resgisterList[0].SetID(i);
+            }
+
+            //
+            //Panel初始化
+            //
+            this.ColumnCount = 1;  //列数
+            this.RowCount = resgisterAmount;  //行数
+            for (int i = 0; i < RowCount; i++)
+            {
+                this.ColumnStyles.Add(new ColumnStyle());
+                this.Controls.Add(resgisterList[i], 0, i);
+            }
+            this.AutoSize = true;
+            //this.BackColor = Color.DarkRed;  //背景颜色
+            this.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;   //边框样式
+        }
+    }
+}
---
+++ PanelUnit/ResgiterCommon.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Panel_Unit.Register.Common
+{
+    public class ResgisterCommon : TableLayoutPanel
+    {
+        //成员
+        private Label ResgisterName = new Label();
+
+        private Label ResgisterValue = new Label();
+        private TextBox ResgisterText = new TextBox();
+        private Button ResgisterButton = new Button();
+
+        //标识特征
+        private int ID;
+
+        //无参构造方法
+        public ResgisterCommon()
+        {
+            //
+            //Panel初始化
+            //
+            this.ColumnCount = 4;  //列数
+            this.RowCount = 1;  //行数
+            this.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150F));
+            this.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 80F));
+            this.ColumnStyles.Add(new ColumnStyle());
+            this.ColumnStyles.Add(new ColumnStyle());
+            this.Size = new Size(390, 35);
+            //this.BackColor = Color.DarkGray;  //背景颜色
+            //
+            //添加成员
+            //
+            this.Controls.Add(this.ResgisterName, 0, 0);
+            this.Controls.Add(this.ResgisterValue, 1, 0);
+            this.Controls.Add(this.ResgisterText, 2, 0);
+            this.Controls.Add(this.ResgisterButton, 3, 0);
+            //
+            // 名称
+            //
+            this.ResgisterName.AutoSize = true;
+            this.ResgisterName.Dock = DockStyle.Fill;
+            this.ResgisterName.TextAlign = ContentAlignment.MiddleRight;
+            this.ResgisterName.Name = "ResgisterName";
+            this.ResgisterName.TabIndex = 2;
+            this.ResgisterName.Text = "参数名称参数名称:";
+            this.ResgisterName.Font = new Font("宋体", 11F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(134)));
+            //this.ResgisterName.BackColor = Color.Red;  //背景颜色
+            //
+            // 读取值
+            //
+            this.ResgisterValue.AutoSize = true;
+            this.ResgisterValue.Dock = DockStyle.Fill;
+            this.ResgisterValue.TextAlign = ContentAlignment.MiddleLeft;
+            this.ResgisterValue.Name = "ResgisterValue";
+            this.ResgisterValue.TabIndex = 2;
+            this.ResgisterValue.Text = "参数数值";
+            this.ResgisterValue.Font = new Font("宋体", 11F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(134)));
+            //this.ResgisterValue.BackColor = Color.Cyan;  //背景颜色
+            //
+            // 输入值填写框
+            //
+            this.ResgisterText.AutoSize = true;
+            this.ResgisterText.Anchor = AnchorStyles.Right;  //设置居中
+            this.ResgisterText.Name = "ResgisterText";
+            this.ResgisterText.TabIndex = 2;
+            //this.ResgisterText.Text = "3333333";
+            this.ResgisterText.Size = new Size(70, 10);  //输入框大小
+            this.ResgisterText.Font = new Font("宋体", 10F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(134)));
+            //this.ResgisterText.BackColor = Color.Green;  //背景颜色
+            //
+            // 按钮
+            //
+            this.ResgisterButton.Dock = DockStyle.Fill;
+            this.ResgisterButton.Anchor = AnchorStyles.Right;
+            this.ResgisterButton.Name = "ResgisterButton";
+            this.ResgisterButton.TabIndex = 2;
+            this.ResgisterButton.Text = "设置";
+            this.ResgisterButton.UseVisualStyleBackColor = true;
+            this.ResgisterButton.FlatStyle = FlatStyle.Popup;
+            this.ResgisterButton.Cursor = Cursors.Hand;
+            this.ResgisterButton.Size = new Size(75, 25);
+            this.ResgisterButton.Click += new EventHandler(this.ResgisterButton_Click);
+        }
+
+        private void ResgisterButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(ResgisterText.Text);
+            ResgisterValue.Text = ResgisterText.Text;
+            ResgisterName.Text = ResgisterText.Text;
+        }
+
+        public void SetID(int ID)
+        {
+            this.ID = ID;
+        }
+
+        public int GetID()
+        {
+            return ID;
+        }
+    }
+}
