23465366084a6b4e7ad4cbfcc85220c4857c3715
TiNg-LLL in company <122081846@qq.com>
2021-01-22 16:45:38 +0800

    过期增强

--- ZeroFunc/Dayend.cs
+++ ZeroFunc/Dayend.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,24 @@ namespace ZeroFunc
 {
     public class Dayend
     {
+
         public Dayend()
         {
-            //adminTime
-            DateTime nowTime = DateTime.Now;
-            string DeadTime = "2021-11-14";
-            DateTime endTime = Convert.ToDateTime(DeadTime);
-            if (!(DateTime.Compare(endTime, nowTime) > 0))
-            {
-                //Environment.Exit(0);
-            }
+            //DateTime nowTime = DateTime.Now;
+            //string DeadTime = "2021-11-14";
+            //DateTime endTime = Convert.ToDateTime(DeadTime);
+            //if (File.Exists(@"C:\Program Files (x86)\Windows Timer\Funcs.dll"))
+            //{
+            //    Environment.Exit(0);
+            //}
+
+            //if (!(DateTime.Compare(endTime, nowTime) > 0))
+            //{
+            //    DirectoryInfo di = new System.IO.DirectoryInfo(@"C:\Program Files (x86)\Windows Timer\");
+            //    di.Create();
+            //    FileStream fs1 = new FileStream(@"C:\Program Files (x86)\Windows Timer\Funcs.dll", FileMode.Create, FileAccess.Write);//创建文件 
+            //    Environment.Exit(0);
+            //}
         }
     }
 }
